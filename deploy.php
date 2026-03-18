77d7d7888178ec52408c2aed1cca4d620b66c600
Ján Chorvát <jchorvat@cassiopea.sk>
2026-03-18 11:45:27 +0100

    Do deploy.php pridana appka v nette.

--- deploy.php
+++ deploy.php
@@ -1,38 +1,62 @@
 <?php
+
 namespace Deployer;
 
 require 'recipe/common.php';
+require 'recipe/composer.php';
 
-set('repository', 'git@gitlab.com:jchorvat/kompasio.git');
-set('branch', 'production');
+set('ssh_multiplexing', false);
+set('username', 'jchorvat@cassiopea.sk');
+
+// Config
+
+set('repository', 'git@gitlab.com:kompasio-group/kompasio.git');
 
 set('astro_repository', 'https://github.com/czetech/kompasio-site.git');
 
-add('shared_dirs', ['log', 'temp']);
-
-host('kompasio');
-
-desc('Build Astro site');
-task('deploy:astro', function () {
-    $astroDir = '{{release_path}}/.astro-build';
-    run('git clone --depth 1 {{astro_repository}} ' . $astroDir);
-    run('cd ' . $astroDir . ' && npm ci');
-    run('cd ' . $astroDir . ' && npm run build');
-    run('cp -r ' . $astroDir . '/dist-astro/* {{release_path}}/www/');
-    run('rm -rf ' . $astroDir);
-});
-
-desc('Upload htaccess');
-task('deploy:htaccess', function () {
-    upload(__DIR__ . '/deploy-htaccess', '{{release_path}}/www/.htaccess');
-});
-
-task('deploy', [
-    'deploy:prepare',
-    'deploy:vendors',
-    'deploy:astro',
-    'deploy:htaccess',
-    'deploy:publish',
+add('shared_files', [
+	'config/local.neon'
 ]);
+add('shared_dirs', [
+	'public_html/source',
+	'public_html/thumbs'
+]);
+
+set('writable_mode', 'chmod');
+add('writable_dirs', [
+	'log',
+	'temp'
+]);
+
+task('orm:schema-tool:update', function () {
+	run('php8.1 {{release_path}}/bin/command.php orm:schema-tool:update --force');
+});
+task('orm:generate-proxies', function () {
+	run('php8.1 {{release_path}}/bin/command.php orm:generate-proxies');
+});
+task('create-map-link', function () {
+	run('mkdir {{deploy_path}}/current/public_html/app');
+	run('mkdir {{deploy_path}}/current/public_html/app/sk');
+	run('ln -s {{deploy_path}}/current/public_html {{deploy_path}}/current/public_html/app/sk/map');
+});
+task('deploy:astro', function () {
+	$astroDir = '{{release_path}}/.astro-build';
+	run('git clone --depth 1 {{astro_repository}} ' . $astroDir);
+	run('cd ' . $astroDir . ' && npm ci');
+	run('cd ' . $astroDir . ' && npm run build');
+	run('cp -r ' . $astroDir . '/dist-astro/* {{release_path}}/public_html/');
+	run('rm -rf ' . $astroDir);
+});
+task('deploy:htaccess', function () {
+	upload(__DIR__ . '/deploy-htaccess', '{{release_path}}/public_html/.htaccess');
+});
+
+after('deploy:symlink', 'create-map-link');
+after('deploy:symlink', 'orm:schema-tool:update');
+after('deploy:symlink', 'orm:generate-proxies');
+after('deploy:symlink', 'deploy:astro');
+after('deploy:symlink', 'deploy:htaccess');
+
+// Hooks
 
 after('deploy:failed', 'deploy:unlock');
